f4a45e6a7d7ed9ad69465d97ff57e3f859af8cb0
sreekala03 <sreekalasn@gmail.com>
2026-07-06 12:56:49 +0200

    11_Transfer_scanvi_labels_to_human_tanycytes_unsupervised.R

--- 7_Transfer_scanvi_labels_to_human_clean.R
+++ 7_Transfer_scanvi_labels_to_human_clean.R
@@ -1,24 +1,22 @@
-#########Generate mouse and human Tanycyte object with scanvi latent embveddings###
-
-library(Seurat)
+#########Transfer scanVI assigned labels to human tanycyte object generated in 5_Human_tanycyte_unsupervised_subclustering to verify the assignments ###
 
 library(Seurat)
 
 # Load Seurat object
 Tanycytes_human_clean <- readRDS(
-  "D:/PROJECTS/snRNseq_integration/human_ME_full/Part2_Tany_human_subset/Tanycytes_human_clean.rds"
+  "/human_ME_full/Part2_Tany_human_subset/Tanycytes_human_clean.rds"
 )
 
 # Load scANVI metadata
 scanvi_metadata <- read.csv(
-  "D:/PROJECTS/snRNseq_integration/human_ME_full/Part3_cross_species/after_scvi/scanvi_metadata_full.csv",
+  "/human_ME_full/Part3_cross_species/after_scvi/scanvi_metadata_full.csv",
   row.names = 1,
   check.names = FALSE
 )
 
 # Load scANVI latent representation
 scanvi_latent <- read.table(
-  "D:/PROJECTS/snRNseq_integration/human_ME_full/Part3_cross_species/after_scvi/scanvi_latent_full.txt",
+  "/human_ME_full/Part3_cross_species/after_scvi/scanvi_latent_full.txt",
   header = TRUE,
   sep = "\t",
   row.names = 1,
@@ -70,60 +68,32 @@ Embeddings(Tanycytes_human_scanvi, "scanvi")[1:5, 1:5]
 # save updated object
 saveRDS(
   Tanycytes_human_scanvi,
-  "D:/PROJECTS/snRNseq_integration/human_ME_full/Part3_cross_species/after_scvi/Tanycytes_human_with_scanvi.rds"
-)
-
-##Add additional cluster resolution
-#human_rds <- file.path(out_dir, "Tanycytes_human_with_scanvi.rds")
-#human_rds <- human
-human <- FindClusters(
-  human, resolution = c(0.4,0.6, 0.8)
-)
-
-DimPlot(
-  human,
-  group.by = "SCT_snn_res.0.4",
-  label = TRUE
-)
-
-DimPlot(
-  human,
-  group.by = "SCT_snn_res.0.6",
-  label = TRUE
-)
-
-DimPlot(
-  human,
-  group.by = "SCT_snn_res.0.8",
-  label = TRUE
+  "/human_ME_full/Part3_cross_species/after_scvi/Tanycytes_human_with_scanvi.rds"
 )
 
 saveRDS(
-  human, "D:/PROJECTS/snRNseq_integration/human_ME_full/Part3_cross_species/after_scvi/Tanycytes_human_with_scanvi.rds"
+  human, "/human_ME_full/Part3_cross_species/after_scvi/Tanycytes_human_with_scanvi.rds"
 )
 
-Tanycytes_human_with_scanvi <- readRDS("D:/PROJECTS/snRNseq_integration/human_ME_full/Part3_cross_species/after_scvi/Tanycytes_human_with_scanvi.rds")
-
-Tanycytes_human_with_scanvi <- readRDS("D:/PROJECTS/snRNseq_integration/human_ME_full/Part3_cross_species/after_scvi/Tanycytes_human_with_scanvi.rds")
 label_col <- "scanvi_final_label"
-Tanycytes_human_with_scanvi$T_cluster <- gsub(
-  "^mT\\.(\\d+)$",
-  "T\\1",
-  as.character(Tanycytes_human_with_scanvi[[label_col]][,1])
-)
+#Tanycytes_human_with_scanvi$T_cluster <- gsub(
+  #"^mT\\.(\\d+)$",
+#  "T\\1",
+ # as.character(Tanycytes_human_with_scanvi[[label_col]][,1])
+#)
 Tanycytes_human_with_scanvi$T_cluster <- factor(
   Tanycytes_human_with_scanvi$T_cluster,
   levels = c("T1", "T2", "T3", "T4", "T5", "T6", "T7", "T8"))
 
 T_cols <- c(
-  "T1"   = "#E69F00",  # gold
-  "T2"   = "#56B4E9",   # blue
-  "T3"   = "#009E73",  # green
-  "T4"   = "#F0E442",  # pink
-  "T5"   = "#0072B2",  # sky blue
-  "T6"   = "#D55E00",  # brown
-  "T7"   = "#F781BF",  # purple
-  "T8"   = "#4D4D4D"   # dark grey
+  "T1"   = "#E69F00",  
+  "T2"   = "#56B4E9",  
+  "T3"   = "#009E73",  
+  "T4"   = "#F0E442", 
+  "T5"   = "#0072B2",  
+  "T6"   = "#D55E00",  
+  "T7"   = "#F781BF",  
+  "T8"   = "#4D4D4D"   
 )
 p_dim =DimPlot(
   Tanycytes_human_with_scanvi,
